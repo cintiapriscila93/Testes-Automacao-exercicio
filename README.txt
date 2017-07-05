cac20b1a625d2c4e047ab0c046095fb668f6f884
Cintia <prigomes@inmetrics.com.br>
2017-07-05 16:06:08 -0300

    First name

---
+++ README.txt
@@ -0,0 +1,15 @@
+Teste automatizado - Soma com numeros aleatorios - Elias Nogueira 
+
+Neste desafio, tente criar um script que some os dois valores e os inclua na caixa de texto. O resultado tem que ser o texto 'Correto'.
+
+Dicas!!!
+
+    Tente pegar o valor dos dois números
+    Tente somá-los com o Selenium
+    Insira o valor somado na caixa de texto
+
+Você vai precisar utilizar, no Selenium IDE, alguns comandos (nao exatamente os que serão listados para nao perder a graça, mas proximo deles):
+
+    assertText
+    store
+
---
+++ src/main/java/br/com/CieloAutomacaoExercicio/AutomacaoSomaAleatoria.java
@@ -0,0 +1,33 @@
+package br.com.CieloAutomacaoExercicio;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
+
+public class AutomacaoSomaAleatoria {
+
+
+public static void main(String[] args) throws InterruptedException {
+
+	// Configurando servidor do Selenium no contexto da aplicação
+	System.setProperty("webdriver.gecko.driver", "C:\\app\\selenium\\geckodriver.exe");
+
+	// Criando Webdriver do Firefox
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://eliasnogueira.com/arquivos_blog/selenium/desafio/1desafio/");
+	
+//Buscando o elemento number1 e number2, que representa os numeros decimais que serão somandos.//
+	String numero1 = driver.findElement(By.id("number1")).getText();
+	String numero2 = driver.findElement(By.id("number2")).getText();
+	
+	//Buscando os elementos soma e submit que representa os campos de realização da soma dos valores decimais e exibição de resultado.// 
+	int resultado = Integer.parseInt(numero1) + Integer.parseInt(numero2);
+	driver.findElement(By.name("soma")).sendKeys(String.valueOf(resultado));
+	driver.findElement(By.name("submit")).click();
+	
+	//Realizando a verificação da mensagem do resultado.//
+	Assert.assertEquals("CORRETO", driver.findElement(By.id("resultado")).getText());
+}
+}
