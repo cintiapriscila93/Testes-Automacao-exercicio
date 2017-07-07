b41f985e3612f201ffa985d2948c20cfe694bb09
Cintia <prigomes@inmetrics.com.br>
2017-07-07 17:31:15 -0300

    firt name

--- src/main/java/br/com/CieloAutomacaoExercicio/IDsDinamicos.java
+++ src/main/java/br/com/CieloAutomacaoExercicio/IDsDinamicos.java
@@ -1,7 +1,10 @@
 package br.com.CieloAutomacaoExercicio;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
 
 public class IDsDinamicos {
 
@@ -13,9 +16,21 @@ public static void main(String[] args) throws InterruptedException {
 		// Criando Webdriver do Firefox
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.eliasnogueira.com/arquivos_blog/selenium/desafio/5desafio/");
-	
-	
-	
+	   // Buscando os elementos que possui id xml dinamicos com o mesmo id, pelo nome do campo, digitando os valores e enviando.//
+		driver.findElement(By.xpath("//label[text() = 'Username']/following-sibling::div/input")).sendKeys("cintia");
+		driver.findElement(By.xpath("//label[text() = 'Username (repetir)']/following-sibling::div/input")).sendKeys("cintia1");
+		driver.findElement(By.xpath("//label[text() = 'Password']/following-sibling::div/input")).sendKeys("12345");
+		driver.findElement(By.xpath("//label[text() = 'Password (repetir)']/following-sibling::div/input")).sendKeys("12345");
+		
+		driver.findElement(By.id("submitBtn2")).click();
+		
+		/*
+		 * Validar o resultado por não ter preenchido o campo username igual
+		 */
+		Assert.assertEquals("Os campos não tem o mesmo valor!", driver.findElement(By.id("submitBtn2")).getText());
+
+	   
+	    
 }
 
 }
